7c5032142e0b3592f5e9f5fa352d39b1df54eb1c
lando2319 <mikepland@gmail.com>
2019-12-09 19:28:44 -0600

    moves game to function, has verdict var

--- randNum.py
+++ randNum.py
@@ -1,16 +1,36 @@
 import random 
+
 print("")
 print("Jason's Random Number Guessing Game") 
-text = raw_input ("Your number is: ")
-print("")
-print("Your number is: ")
-print(text)
-print("") 
-outputNumber = random.randint(1,1)
 
-print("The Game Chose: ", outputNumber)
 
-if (int(text) == outputNumber): 
-	print(text, "is correct")
-else:
-	print(text, "is incorrect")
+# roundNum = 0
+
+def runGame():
+    print("")
+    print("READY FOR ROUND") 
+
+    text = raw_input ("Your number is: ")
+    print("")
+    print("Your number is: ")
+    print(text)
+    print("") 
+    outputNumber = random.randint(1,1)
+
+    print("The Game Chose: ", outputNumber)
+
+    verdict = ""
+
+    if (int(text) == outputNumber): 
+        verdict = "WON"
+    else:
+        verdict = "LOST"
+
+    # print("YOU HAVE ", verdict, "Round", roundNum)
+    print("YOU HAVE " + verdict + " Round")
+    # roundNum = roundNum + 1
+    return runGame()
+
+
+
+runGame()
